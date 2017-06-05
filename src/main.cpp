97cf542fdb0c2299d5499dd56469c0523e0c6c4a
marksteven2017 <marksteven2017@users.noreply.github.com>
2017-06-05 18:08:27 +0200

    Update main.cpp
    +

--- src/main.cpp
+++ src/main.cpp
@@ -1100,7 +1100,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     return nSubsidy + nFees;
 }
 
-static const int64 nTargetTimespan = 10 * 60;
+static const int64 nTargetTimespan = 1 * 60;
 static const int64 nTargetSpacing = 1 * 60;
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
